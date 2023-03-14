7e0998c36f0f76b191241da7ecbfff289a42a427
Mohamad Farhanuddin Firaq <mfarhanuf@gmail.com>
2023-03-14 15:19:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 
 k="Created with an evaluation copy of Aspose.Words. To discover the full versions of our APIs please visit: https://products.aspose.com/words/"
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 @app.post("/files/")
